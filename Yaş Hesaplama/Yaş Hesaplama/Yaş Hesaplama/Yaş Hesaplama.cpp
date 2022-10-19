d37b1e483ebb46a21849a21f85579fb33098e67c
Recep Enes <recepenskaya@gmail.com>
2022-10-19 21:20:25 +0300

    yashesapla

---
+++ Yaş Hesaplama/Yaş Hesaplama/Yaş Hesaplama/Yaş Hesaplama.cpp
@@ -0,0 +1,19 @@
+﻿// Yaş Hesaplama.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int Dogumyili;
+    int yas;
+    printf("*Yas Hesaplama Programina Hosgeldiniz*\n");
+    printf("Lutfen Dogum Yilinizi giriniz: ");
+    scanf_s("%d", &Dogumyili);
+    yas = 2022 - Dogumyili;
+    printf("Kullanicinin Yasi: %d", yas);
+
+    
+    
+}
+
